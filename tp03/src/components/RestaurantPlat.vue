7cd3304a3b2abd0c46f91cd07148afeeb23dd2f3
Derek <derek.samson@etu.unice.fr>
2019-12-31 14:19:14 +0100

    corrections to RestaurantPlats

--- tp03/src/components/RestaurantPlat.vue
+++ tp03/src/components/RestaurantPlat.vue
@@ -7,21 +7,21 @@
         <tr>
           <b>Hors d'Oeuvres</b>
         </tr>
-        <tr v-for="(m, index) in menus[0].horsdoeuvres" :key="index">
+        <tr v-for="(m, index) in menus[0].horsdoeuvres" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Plats</b>
         </tr>
-        <tr v-for="(m, index) in menus[0].plats" :key="index">
+        <tr v-for="(m, index) in menus[0].plats" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Desserts</b>
         </tr>
-        <tr v-for="(m, index) in menus[0].desserts" :key="index">
+        <tr v-for="(m, index) in menus[0].desserts" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
@@ -37,21 +37,21 @@
         <tr>
           <b>Hors d'Oeuvres</b>
         </tr>
-        <tr v-for="(m, index) in menus[1].horsdoeuvres" :key="index">
+        <tr v-for="(m, index) in menus[1].horsdoeuvres" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Plats</b>
         </tr>
-        <tr v-for="(m, index) in menus[1].plats" :key="index">
+        <tr v-for="(m, index) in menus[1].plats" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Desserts</b>
         </tr>
-        <tr v-for="(m, index) in menus[1].desserts" :key="index">
+        <tr v-for="(m, index) in menus[1].desserts" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
@@ -65,21 +65,21 @@
         <tr>
           <b>Hors d'Oeuvres</b>
         </tr>
-        <tr v-for="(m, index) in menus[2].horsdoeuvres" :key="index">
+        <tr v-for="(m, index) in menus[2].horsdoeuvres" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Plats</b>
         </tr>
-        <tr v-for="(m, index) in menus[2].plats" :key="index">
+        <tr v-for="(m, index) in menus[2].plats" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
         <tr>
           <b>Desserts</b>
         </tr>
-        <tr v-for="(m, index) in menus[2].desserts" :key="index">
+        <tr v-for="(m, index) in menus[2].desserts" :key="`${index}-${m[0]}`">
           <td>{{ m[0] }}</td>
           <td>{{ m[4] }}</td>
         </tr>
@@ -182,26 +182,14 @@ export default {
   },
   methods: {
     createMenus(obj, n) {
-      //let menuCuisine = `Menu ${this.cuisine}`;
       let menus = [];
       for (let i = 0; i < n; i++) {
         let menu = { horsdoeuvres: {}, plats: {}, desserts: {} };
-        let hd = Array(n)
-          .fill()
-          .map(
-            () =>
-              obj.horsdoeuvres[
-                Math.floor(Math.random() * obj.horsdoeuvres.length)
-              ]
-          );
-        let plats = Array(n)
-          .fill()
-          .map(() => obj.plats[Math.floor(Math.random() * obj.plats.length)]);
-        let desserts = Array(n)
-          .fill()
-          .map(
-            () => obj.desserts[Math.floor(Math.random() * obj.desserts.length)]
-          );
+        let arrLen = obj.horsdoeuvres.length;
+        let randNum = Array.from(Array(arrLen), (e, i) => i).slice(0, 3);
+        let hd = [...randNum].map(i => obj.horsdoeuvres[randNum[i]]);
+        let plats = [...randNum].map(i => obj.plats[randNum[i]]);
+        let desserts = [...randNum].map(i => obj.desserts[randNum[i]]);
         menu.horsdoeuvres = hd;
         menu.plats = plats;
         menu.desserts = desserts;
