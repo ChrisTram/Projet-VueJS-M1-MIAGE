cb45fdd33a36330b5b9a46efaac1f7ec7e712063
Derek <derek.samson@etu.unice.fr>
2019-12-31 16:49:11 +0100

    added price to menu

--- tp03/src/components/RestaurantMenu.vue
+++ tp03/src/components/RestaurantMenu.vue
@@ -1,92 +1,103 @@
 <template>
-  <div>
+  <div style="margin-left:auto;margin-right:auto; width:40%;">
     <h1>Menu {{ cuisine }}</h1>
-    <table style="margin-left:auto;margin-right:auto;">
-      <tr></tr>
+    <md-table md-sort="name" md-sort-order="asc" md-card>
+      <md-table-row></md-table-row>
       <tbody>
-        <tr>
+        <md-table-row>
           <b>Hors d'Oeuvres</b>
-        </tr>
-        <tr v-for="(m, index) in menus[0].horsdoeuvres" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+
+        <md-table-row v-for="(m, index) in menus[0].horsdoeuvres" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Plats</b>
-        </tr>
-        <tr v-for="(m, index) in menus[0].plats" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[0].plats" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Desserts</b>
-        </tr>
-        <tr v-for="(m, index) in menus[0].desserts" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[0].desserts" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
+          <b>Prix</b>
+        </md-table-row>
+        <md-table-row>
+          <md-table-cell>{{ getMenuPrice(menus[0]) }}</md-table-cell>
+        </md-table-row>
       </tbody>
-    </table>
+    </md-table>
     <br />
     <h1>
       <b>Menu Classique</b>
     </h1>
-    <table style="margin-left:auto;margin-right:auto;">
-      <tr></tr>
+    <md-table style="margin-left:auto;margin-right:auto;">
+      <md-table-row></md-table-row>
       <tbody>
-        <tr>
+        <md-table-row>
           <b>Hors d'Oeuvres</b>
-        </tr>
-        <tr v-for="(m, index) in menus[1].horsdoeuvres" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[1].horsdoeuvres" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Plats</b>
-        </tr>
-        <tr v-for="(m, index) in menus[1].plats" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[1].plats" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Desserts</b>
-        </tr>
-        <tr v-for="(m, index) in menus[1].desserts" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[1].desserts" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
+          <b>Prix</b>
+        </md-table-row>
+        <md-table-row>
+          <md-table-cell>{{ getMenuPrice(menus[1]) }}</md-table-cell>
+        </md-table-row>
       </tbody>
-    </table>
+    </md-table>
     <br />
     <h1>Menu Alternatif</h1>
-    <table style="margin-left:auto;margin-right:auto;">
-      <tr></tr>
+    <md-table style="margin-left:auto;margin-right:auto;">
+      <md-table-row></md-table-row>
       <tbody>
-        <tr>
+        <md-table-row>
           <b>Hors d'Oeuvres</b>
-        </tr>
-        <tr v-for="(m, index) in menus[2].horsdoeuvres" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[2].horsdoeuvres" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Plats</b>
-        </tr>
-        <tr v-for="(m, index) in menus[2].plats" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
-        <tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[2].plats" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
           <b>Desserts</b>
-        </tr>
-        <tr v-for="(m, index) in menus[2].desserts" :key="`${index}-${m[0]}`">
-          <td>{{ m[0] }}</td>
-          <td>{{ m[4] }}</td>
-        </tr>
+        </md-table-row>
+        <md-table-row v-for="(m, index) in menus[2].desserts" :key="`${index}-${m[0]}`">
+          <md-table-cell>{{ m[0] }}</md-table-cell>
+        </md-table-row>
+        <md-table-row>
+          <b>Prix</b>
+        </md-table-row>
+        <md-table-row>
+          <md-table-cell>{{ getMenuPrice(menus[2]) }}</md-table-cell>
+        </md-table-row>
       </tbody>
-    </table>
+    </md-table>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -119,6 +130,22 @@ export default {
         menus.push(menu);
       }
       return menus;
+    },
+    getMenuPrice(menu) {
+      let totalHD = menu.horsdoeuvres.reduce(
+        (e, i) => e + parseFloat(i[4].slice(0, -1).replace(",", ".")),
+        0
+      );
+      let totalPlats = menu.plats.reduce(
+        (e, i) => e + parseFloat(i[4].slice(0, -1).replace(",", ".")),
+        0
+      );
+      let totalDesserts = menu.desserts.reduce(
+        (e, i) => e + parseFloat(i[4].slice(0, -1).replace(",", ".")),
+        0
+      );
+      let total = totalHD + totalPlats + totalDesserts;
+      return total - (total * 10) / 100 + "€";
     }
   }
 };
